27a9908cad54f44c21aa3ba0ea7a569415cea772
Tobias Svensson <tobsve03@gmail.com>
2026-09-08 21:47:13 +0200

    Comments cleanup

--- src/audiofx/asm/echo_asm.s
+++ src/audiofx/asm/echo_asm.s
@@ -14,24 +14,24 @@ func    AFX_echo_process64_ASM
         movem.l a0-a2/d0-d6,-(sp)
 
 afx_echo_init:
-* samplesPtr -> a0
+        * samplesPtr -> a0
         movea.l 44(sp),a0
-* linePtr    -> a1
+        * linePtr    -> a1
         movea.l 52(sp),a1
 
-* size       -> s0
+        * size       -> d0
         move.l  48(sp),d0
-* linePos        -> s0
+        * linePos    -> d1
         move.l  56(sp),d1
-* lineLen        -> s0
+        * lineLen    -> d2
         move.l  60(sp),d2
 
 
-* linePtrAt  -> a2
+        * linePtrAt  -> a2
         move.l  a1,a2
         add.l   d1,a2
 
-* linePtrEnd -> d5
+        * linePtrEnd -> d5
         move.l  a1,d5
         add.l   d2,d5
 
@@ -41,11 +41,13 @@ afx_echo_loop:
 .L1:
         afx8_echo_doProcess64
 
+        * Wrap line ptr
         cmpa.l  d5,a2
         bcs     .L2
         move.l  a1,a2
 
 .L2:
+        * Loop
         subi.w  #64,d0
         bne     .L1
 
