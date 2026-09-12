b5714ca3b1e564e0ff5e3d8bdac2677b9dc0fc9f
Tobias Svensson <tobsve03@gmail.com>
2026-09-12 21:55:08 +0200

    Move lineMask onto stack

--- src/audiofx/asm/reverb_asm.s
+++ src/audiofx/asm/reverb_asm.s
@@ -9,19 +9,27 @@
 * );
 
 func    AFX_reverb_process
-        movem.l d2-d7,-(sp)
+        movem.l a2/d2-d7,-(sp)
 
 afx_reverb_init:
         * samplesPtr   -> a0
-        movea.l 28(sp),a0
+        movea.l 32(sp),a0
         * afxPtr       -> a1
-        movea.l 36(sp),a1
+        movea.l 40(sp),a1
+
+        * lineMaskTemp -> d1
+        move.w  4(a1),d1
+        subq.w  #1,d1
+
+        * lineMaskPtr  -> a2
+        move.w  4(a1),d1
+        subq.w  #1,d1
+        lea     -2(sp),a2
+        move.w  d1,(a2)
 
         * linePos      -> d0
         move.w  6(a1),d0
-        * lineMask     -> d1
-        move.w  4(a1),d1
-        subq.w  #1,d1
+
         * delayPos1    -> d2
         move.l  d0,d2
         sub.w   8(a1),d2
@@ -30,15 +38,37 @@ afx_reverb_init:
         move.l  d0,d3
         sub.w   10(a1),d3
         and.w   d1,d3
+        * delayPos3    -> d4
+        move.l  d0,d4
+        sub.w   12(a1),d4
+        and.w   d1,d4
+        * delayPos4    -> d5
+        move.l  d0,d5
+        sub.w   14(a1),d5
+        and.w   d1,d5
 
-
+        * delayAccum   -> d1
 
         * calc0        -> d6
 
         * calc1        -> d7
 
-        * linePtr      -> a1
+        * delayLinePtr -> a1
         movea.l (a1),a1
+
+        * -- Result of init is: --
+        * samplesPtr   -> a0
+        * delayLinePtr -> a1
+        * lineMaskPtr  -> a2
+
+        * linePos      -> d0
+        * delayAccum   -> d1
+        * delayPos1    -> d2
+        * delayPos2    -> d3
+        * delayPos3    -> d4
+        * delayPos4    -> d5
+        * calc0        -> d6
+        * calc1        -> d7
 
 afx_reverb_loop:
 
@@ -46,13 +76,13 @@ afx_reverb_loop:
         afx8_reverb_doProcess64
 
         * Loop
-        subi.w  #64,34(sp)
+        subi.w  #64,38(sp)
         bne     .L1
 
 afx_reverb_return:
         * Write back pos
-        movea.l 36(sp),a1
+        movea.l 40(sp),a1
         move.w  d0,6(a1)
 
-        movem.l (sp)+,d2-d7
+        movem.l (sp)+,a2/d2-d7
         rts
--- src/audiofx/asm/reverb_mac.i
+++ src/audiofx/asm/reverb_mac.i
@@ -1,13 +1,13 @@
-
-.macro  afx8_reverb_halveAndMixDelaySample
-        * -- Delay sample is in d6
-
+.macro  afx8_reverb_fixMinusOneDelaySampleD6
         * Fix for 0xFF >> 1 = 0xFF (-1 >> 1 = -1) leaving behind a noise floor in delay line
         move.l  d6,d7
         and.l   #0x80808080,d7
         lsr.l   #7,d7
         add.l   d7,d6
-        * '60c
+.endm
+
+.macro  afx8_reverb_halveAndMixDelaySampleD6
+        afx8_reverb_fixMinusOneDelaySampleD6
 
         * Get delay line sign bits into d7
         move.l  d6,d7
@@ -28,19 +28,19 @@
 .macro  afx8_reverb_doProcess4
         * Process delay line sample 1
         move.l  (a1,d2.w),d6
-        afx8_reverb_halveAndMixDelaySample
+        afx8_reverb_halveAndMixDelaySampleD6
 
         * Process delay line sample 2
         move.l  (a1,d3.w),d6
-        afx8_reverb_halveAndMixDelaySample
+        afx8_reverb_halveAndMixDelaySampleD6
 
         * Process delay line sample 3
         move.l  (a1,d4.w),d6
-        afx8_reverb_halveAndMixDelaySample
+        afx8_reverb_halveAndMixDelaySampleD6
 
         * Process delay line sample 4
         move.l  (a1,d5.w),d6
-        afx8_reverb_halveAndMixDelaySample
+        afx8_reverb_halveAndMixDelaySampleD6
 
         * Write result in out to delay line
         move.l  (a0)+,(a1,d0.w)
@@ -49,6 +49,7 @@
         addq.w  #4,d0
         addq.w  #4,d2
         addq.w  #4,d3
+        move.w  (a2),d1
         and.w   d1,d0
         and.w   d1,d2
         and.w   d1,d3
